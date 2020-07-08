00e5b5b9cfcf57eb1f59f68cef74a4bf50301f78
Gabriel Silva <gabrielsos99@gmail.com>
2020-07-08 02:07:38 -0300

    completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
 }
 
 class TransactionsRepository {
@@ -14,15 +21,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    if (!this.transactions) {
+      throw Error('No data found!');
+    }
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(transaction => transaction.value)
+      .reduce((total: number, values: number) => total + values, 0);
+
+    const totalOutcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(transaction => transaction.value)
+      .reduce((total: number, values: number) => total + values, 0);
+
+    const total = totalIncome - totalOutcome;
+
+    const balanceObject = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return balanceObject;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const transaction = { id: uuid(), title, type, value };
+
+    this.transactions.push(transaction);
+
+    if (!transaction) {
+      throw Error('Erro ao criar!');
+    }
+    return transaction;
   }
 }
 
